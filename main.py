c2950b0ea479e4d024162cea54bb49bfd162f7a5
chipdelmal <chipdelmal@gmail.com>
2024-10-04 15:39:00 -0700

    Code runs and moves files correctly

---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import shutil as sht
+from glob import glob
+from os import path, makedirs
+from time import ctime, strptime, strftime
+# from tqdm import tqdm
+
+
+(PTH_I, PTH_O) = (
+    path.join('/Volumes/LUMIX', 'DCIM/100_PANA'),
+    path.join('/Users/chipdelmal/Pictures/Lumix')
+)
+MOVE = True
+###############################################################################
+# Get filenames and iterate over them
+###############################################################################
+fnames = glob(path.join(PTH_I, '*'))
+for fname in fnames:
+    print(f'\033[94m * Processing {fname}\033[0m')
+    ###########################################################################
+    # Get file datetime
+    ###########################################################################
+    (cTime, mTime) = (
+        strptime(ctime(path.getctime(fname))),
+        strptime(ctime(path.getmtime(fname)))
+    )
+    ###########################################################################
+    # Create output folder
+    ###########################################################################
+    fldrNme = strftime("%Y_%m_%d", cTime)
+    fldrOut = path.join(PTH_O, fldrNme)
+    if not path.exists(fldrOut):
+        makedirs(fldrOut)
+    fnameOut = path.join(fldrOut, path.split(fname)[-1])
+    ###########################################################################
+    # Move or Copy files
+    ###########################################################################
+    if MOVE:
+        sht.move(fname, fnameOut)
+    else:
+        sht.copy(fname, fnameOut)
