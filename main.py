2644661e94a46d9872fb39c23bcda6660735889b
chipdelmal <chipdelmal@gmail.com>
2024-10-26 13:59:54 -0700

    Split by filetype

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ from time import ctime, strptime, strftime
 
 (PTH_I, PTH_O) = (
     path.join('/Volumes/LUMIX', 'DCIM/100_PANA'),
-    path.join('/Users/chipdelmal/Pictures/Lumix')
+    # path.join('/Users/chipdelmal/Pictures/Lumix')
+    path.join('/Volumes/Kai/Lumix')
 )
-MOVE = True
+MOVE = False
 ###############################################################################
 # Get filenames and iterate over them
 ###############################################################################
 fnames = glob(path.join(PTH_I, '*'))
 for fname in fnames:
     print(f'\033[94m * Processing {fname}\033[0m')
+    ###########################################################################
+    # Get file extensions
+    ###########################################################################
+    fext = path.splitext(fname)[-1][1:]
     ###########################################################################
     # Get file datetime
     ###########################################################################
@@ -29,7 +34,7 @@ for fname in fnames:
     # Create output folder
     ###########################################################################
     fldrNme = strftime("%Y_%m_%d", cTime)
-    fldrOut = path.join(PTH_O, fldrNme)
+    fldrOut = path.join(PTH_O, fldrNme, fext)
     if not path.exists(fldrOut):
         makedirs(fldrOut)
     fnameOut = path.join(fldrOut, path.split(fname)[-1])
